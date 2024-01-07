9e5059890c2725a2aaaf7397731264363d46a85e
tony0325 <tony0325@gmail.com>
2024-01-07 23:16:18 +0800

    Update scd40.ino

--- scd40.ino
+++ scd40.ino
@@ -1,4 +1,6 @@
 #include <Wire.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <SparkFun_SCD4x_Arduino_Library.h>
@@ -6,31 +8,41 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
+const char* ssid = "OnHub"; // 替換為您的WiFi網絡名稱
+const char* password = "1122334455"; // 替換為您的WiFi密碼
+const char* googleScriptURL = "https://script.google.com/macros/s/AKfycbxY7Db6Yv6yMEPBcgiSxhRzKM-7cHiWWEVHwwhEPYY06W5B5E5WpiPqFO-yV00a71b3/exec"; // 替換為您的Google Apps Script的URL
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
 SCD4x mySensor;
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("\n");
   Wire.begin();
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-
-  if (!mySensor.begin()) {
-    display.println("SCD4x not found");
-    display.display();
-    while (1);
-  }
-
-  // Start continuous measurement
-  mySensor.startPeriodicMeasurement();
-}
-
-void loop() {
-  // Assuming the library handles data availability internally
-  mySensor.readMeasurement();
-
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
+
+  if (!mySensor.begin()) {
+    Serial.println("SCD4x not found");
+    while (1);
+  }
+
+  mySensor.startPeriodicMeasurement();
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(2000);
+    Serial.println("Connecting to WiFi...");
+    display.display();
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  mySensor.readMeasurement();
   display.print("CO2:");
   display.print(mySensor.getCO2());
   display.println("ppm");
@@ -44,6 +56,31 @@ void loop() {
   display.println("%");
 
   display.display();
-  
-  delay(2000); // Update every 2 seconds
+
+  if(WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+    String url = String(googleScriptURL) + "?temperature=" + mySensor.getTemperature() +
+                 "&humidity=" + mySensor.getHumidity() + 
+                 "&co2=" + mySensor.getCO2();
+    
+    
+    http.begin(url);
+
+    int httpCode = http.GET();
+
+    display.clearDisplay();
+    display.setCursor(0, 0);
+
+    if (httpCode > 0) {
+      String payload = http.getString();
+      Serial.println("Data sent successfully");
+    } else {
+      Serial.println("Error sending data");
+      Serial.println(httpCode);
+    }
+
+    http.end();
+  }
+
+  delay(60000); // Update every 60 seconds
 }
